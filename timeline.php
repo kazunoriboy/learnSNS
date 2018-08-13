76b36de0fccf3e02732d037801f0fe393c8635ea
松島一徳 <>
2018-08-13 14:11:58 +0800

    ログインユーザーの画像と名前を表示

--- timeline.php
+++ timeline.php
@@ -1,7 +1,86 @@
 <?php
+  session_start();
+  require('dbconnect.php');
+  //ログインユーザー情報の取得
+  $sql = 'SELECT * FROM `users` WHERE `id`=?';
+  $data = array($_SESSION["id"]);
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
 
-session_start();
+  $login_user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-var_dump($_SESSION);
-echo 'とんだよ';
+var_dump($login_user);
+
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Learn SNS</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body style="margin-top: 60px; background: #E4E6EB;">
+  <?php include('nav.php'); ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li class="active"><a href="timeline.php?feed_select=news">新着順</a></li>
+          <li><a href="timeline.php?feed_select=likes">いいね！済み</a></li>
+          <!-- <li><a href="timeline.php?feed_select=follows">フォロー</a></li> -->
+        </ul>
+      </div>
+      <div class="col-xs-9">
+        <div class="feed_form thumbnail">
+          <form method="POST" action="">
+            <div class="form-group">
+              <textarea name="feed" class="form-control" rows="3" placeholder="Happy Hacking!" style="font-size: 24px;"></textarea><br>
+            </div>
+            <input type="submit" value="投稿する" class="btn btn-primary">
+          </form>
+        </div>
+          <div class="thumbnail">
+            <div class="row">
+              <div class="col-xs-1">
+                <img src="https://placehold.jp/40x40.png" width="40">
+              </div>
+              <div class="col-xs-11">
+                野原ひろし<br>
+                <a href="#" style="color: #7F7F7F;">2018-03-03</a>
+              </div>
+            </div>
+            <div class="row feed_content">
+              <div class="col-xs-12" >
+                <span style="font-size: 24px;">夢は逃げない。逃げるのはいつも自分だ。</span>
+              </div>
+            </div>
+            <div class="row feed_sub">
+              <div class="col-xs-12">
+                <form method="POST" action="" style="display: inline;">
+                  <input type="hidden" name="feed_id" >
+                    <input type="hidden" name="like" value="like">
+                    <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
+                </form>
+                <span class="like_count">いいね数 : 100</span>
+                <span class="comment_count">コメント数 : 9</span>
+                  <a href="#" class="btn btn-success btn-xs">編集</a>
+                  <a href="#" class="btn btn-danger btn-xs">削除</a>
+              </div>
+            </div>
+          </div>
+        <div aria-label="Page navigation">
+          <ul class="pager">
+            <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+            <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
